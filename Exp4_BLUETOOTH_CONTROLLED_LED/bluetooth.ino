ae6c7df7c5e19cdae36c4cfe67de05255960aab9
Karan-Choudhary <54716931+Karan-Choudhary@users.noreply.github.com>
2019-10-20 13:23:45 +0530

    Update bluetooth.ino

--- Exp4_BLUETOOTH_CONTROLLED_LED/bluetooth.ino
+++ Exp4_BLUETOOTH_CONTROLLED_LED/bluetooth.ino
@@ -1,7 +1,7 @@
 char data = 0;
 void setup()
 {
-pinMode(2,OUTPUT);
+pinMode(13,OUTPUT);
 Serial.begin(9600);
 }
 void loop()
@@ -11,11 +11,11 @@ if(Serial.available()>0)
 data=Serial.read();
 if(data=='1')
 {
-digitalWrite(2,HIGH);
+digitalWrite(13,HIGH);
 }
 else if(data=='0')
 {
-digitalWrite(2,LOW);
+digitalWrite(13,LOW);
 }
 }
 }
